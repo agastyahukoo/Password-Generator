89d56c10121109c81fa313e92051e5254ef77614
Agastya Hukoo <136306593+agastya-hukoo@users.noreply.github.com>
2023-07-20 18:18:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.IO;
+
+class P
+{
+    private const string A = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+    private readonly R B;
+
+    public P()
+    {
+        B = new R();
+    }
+
+    public string C(int D)
+    {
+        char[] E = new char[D];
+
+        for (int F = 0; F < D; F++)
+        {
+            E[F] = A[B.G(A.Length)];
+        }
+
+        return new string(E);
+    }
+}
+
+class Q
+{
+    static void Main(string[] G)
+    {
+
+        while (true)
+        {
+            string asciiText = @"
+██████╗░░█████╗░░██████╗░██████╗░██╗░░░░░░░██╗░█████╗░██████╗░██████╗░
+██╔══██╗██╔══██╗██╔════╝██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔══██╗
+██████╔╝███████║╚█████╗░╚█████╗░░╚██╗████╗██╔╝██║░░██║██████╔╝██║░░██║
+██╔═══╝░██╔══██║░╚═══██╗░╚═══██╗░░████╔═████║░██║░░██║██╔══██╗██║░░██║
+██║░░░░░██║░░██║██████╔╝██████╔╝░░╚██╔╝░╚██╔╝░╚█████╔╝██║░░██║██████╔╝
+╚═╝░░░░░╚═╝░░╚═╝╚═════╝░╚═════╝░░░░╚═╝░░░╚═╝░░░╚════╝░╚═╝░░╚═╝╚═════╝░
+
+░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░████████╗░█████╗░██████╗░
+██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║░░░██║░░░██║░░██║██████╔╝
+██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║░░░██║░░░██║░░██║██╔══██╗
+╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝";
+
+            Console.WriteLine(asciiText);
+            Console.WriteLine("Password Generator v1.0");
+            Console.WriteLine("------------------");
+            Console.WriteLine("Thank you for using Password Generator v1.0");
+
+
+            Console.WriteLine("Main Menu:");
+            Console.WriteLine("1. Generate a new password");
+            Console.WriteLine("2. View saved passwords");
+            Console.WriteLine("3. Exit");
+            Console.Write("Enter your choice (1, 2, or 3): ");
+            string H = Console.ReadLine();
+
+            if (H == "1")
+            {
+                I();
+            }
+            else if (H == "2")
+            {
+                J();
+            }
+            else if (H == "3")
+            {
+                break;
+            }
+
+            Console.Clear();
+        }
+    }
+
+    static void I()
+    {
+        Console.Clear();
+        string asciiText = @"
+██████╗░░█████╗░░██████╗░██████╗░██╗░░░░░░░██╗░█████╗░██████╗░██████╗░
+██╔══██╗██╔══██╗██╔════╝██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔══██╗
+██████╔╝███████║╚█████╗░╚█████╗░░╚██╗████╗██╔╝██║░░██║██████╔╝██║░░██║
+██╔═══╝░██╔══██║░╚═══██╗░╚═══██╗░░████╔═████║░██║░░██║██╔══██╗██║░░██║
+██║░░░░░██║░░██║██████╔╝██████╔╝░░╚██╔╝░╚██╔╝░╚█████╔╝██║░░██║██████╔╝
+╚═╝░░░░░╚═╝░░╚═╝╚═════╝░╚═════╝░░░░╚═╝░░░╚═╝░░░╚════╝░╚═╝░░╚═╝╚═════╝░
+
+░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░████████╗░█████╗░██████╗░
+██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║░░░██║░░░██║░░██║██████╔╝
+██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║░░░██║░░░██║░░██║██╔══██╗
+╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝";
+
+        Console.WriteLine(asciiText);
+        Console.WriteLine("Generate New Password");
+        Console.WriteLine("---------------------");
+
+        P K = new P();
+
+        Console.Write("Enter the length of the password: ");
+        int L = int.Parse(Console.ReadLine());
+        string N = K.C(L);
+        Console.WriteLine("Generated Password: " + N);
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("1. Save the password");
+        Console.WriteLine("2. Do not save the password");
+        Console.Write("Enter your choice (1 or 2): ");
+        string M = Console.ReadLine();
+
+
+
+        if (M == "1")
+        {
+            O(N);
+        }
+
+        Console.WriteLine("Press Enter to continue...");
+        Console.ReadLine();
+    }
+
+    static void O(string P)
+    {
+        Console.Write("Enter a name for the password: ");
+        string Q = Console.ReadLine();
+
+        string R = $"{Q}: {P}";
+
+        try
+        {
+            using (StreamWriter S = File.AppendText("passwords.txt"))
+            {
+                S.WriteLine(R);
+            }
+
+            Console.WriteLine("Password saved successfully!");
+        }
+        catch (Exception T)
+        {
+            Console.WriteLine("An error occurred while saving the password: " + T.Message);
+        }
+    }
+
+    static void J()
+    {
+        Console.Clear();
+        string asciiText = @"
+██████╗░░█████╗░░██████╗░██████╗░██╗░░░░░░░██╗░█████╗░██████╗░██████╗░
+██╔══██╗██╔══██╗██╔════╝██╔════╝░██║░░██╗░░██║██╔══██╗██╔══██╗██╔══██╗
+██████╔╝███████║╚█████╗░╚█████╗░░╚██╗████╗██╔╝██║░░██║██████╔╝██║░░██║
+██╔═══╝░██╔══██║░╚═══██╗░╚═══██╗░░████╔═████║░██║░░██║██╔══██╗██║░░██║
+██║░░░░░██║░░██║██████╔╝██████╔╝░░╚██╔╝░╚██╔╝░╚█████╔╝██║░░██║██████╔╝
+╚═╝░░░░░╚═╝░░╚═╝╚═════╝░╚═════╝░░░░╚═╝░░░╚═╝░░░╚════╝░╚═╝░░╚═╝╚═════╝░
+
+░██████╗░███████╗███╗░░██╗███████╗██████╗░░█████╗░████████╗░█████╗░██████╗░
+██╔════╝░██╔════╝████╗░██║██╔════╝██╔══██╗██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+██║░░██╗░█████╗░░██╔██╗██║█████╗░░██████╔╝███████║░░░██║░░░██║░░██║██████╔╝
+██║░░╚██╗██╔══╝░░██║╚████║██╔══╝░░██╔══██╗██╔══██║░░░██║░░░██║░░██║██╔══██╗
+╚██████╔╝███████╗██║░╚███║███████╗██║░░██║██║░░██║░░░██║░░░╚█████╔╝██║░░██║
+░╚═════╝░╚══════╝╚═╝░░╚══╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚════╝░╚═╝░░╚═╝";
+
+        Console.WriteLine(asciiText);
+        Console.WriteLine("View Saved Passwords");
+        Console.WriteLine("--------------------");
+
+        try
+        {
+            using (StreamReader U = File.OpenText("passwords.txt"))
+            {
+                string V;
+                while ((V = U.ReadLine()) != null)
+                {
+                    Console.WriteLine(V);
+                }
+            }
+
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadLine();
+        }
+        catch (Exception W)
+        {
+            Console.WriteLine("An error occurred while reading the passwords: " + W.Message);
+        }
+    }
+}
+
+class R
+{
+    private readonly Random X;
+
+    public R()
+    {
+        X = new Random();
+    }
+
+    public int G(int Y)
+    {
+        return X.Next(Y);
+    }
+}
